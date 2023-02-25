055ded71432cc1799470373620e510673078bf1a
Paguiar735 <72931357+Paguiar735@users.noreply.github.com>
2023-02-24 21:52:11 -0400

    [refactor]: improve readability of client code

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import requests
 
-files = {
-    'file': open('./mre/dummy_doc.docx', 'rb'),
-}
-
-r = requests.post('http://localhost:5000/doc_to_pdf', files=files)
-
+endpoint = "http://localhost:5000/doc_to_pdf"
+path_to_doc = './mre/dummy_doc.docx'
 chunk_size = 2000
+
+r = requests.post(endpoint, files={
+    'file': open(path_to_doc, 'rb'),
+})
+
 with open('output.pdf', 'wb') as fd:
     for chunk in r.iter_content(chunk_size):
         fd.write(chunk)
